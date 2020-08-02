5aa22a4c457fd4b3c95ebf148717666d1056be7c
kirill <atray@mail.ru>
2020-08-02 13:36:21 +0300

    Initial commit

---
+++ src/main/java/PhotoPathParser.java
@@ -0,0 +1,12 @@
+public class PhotoPathParser {
+
+    public static void main(String[] args) {
+
+        //incomming args are source path and target path
+        /*
+        1. parser class and fileVisitor
+        2. DB JDBC class connector
+         */
+        }
+    }
+}
